ff16e5ab431de0e060b4c2cfe61d75e5215cadbc
Roman Rader <roman.rader@gmail.com>
2015-10-22 18:51:31 +0300

    full deploy

--- fabfile.py
+++ fabfile.py
@@ -7,28 +7,9 @@ REPO = 'https://github.com/rrader/events-service.git'
 BASE_TARGET_PATH = '/opt'
 
 
-def host_type():
-    run('uname -s')
-
-
 @runs_once
 def prepare_packages():
     run('apt-get update')
-
-
-def require_packages(*names):
-    for name in names:
-        with settings(warn_only=True):
-            if run('dpkg -s {}'.format(name)).return_code != 0:
-                prepare_packages()
-                run('apt-get install -y {}'.format(name))
-
-
-def require_vagga():
-    with settings(warn_only=True):
-        if run('which vagga').return_code != 0:
-            require_packages('curl')
-            run('curl http://files.zerogw.com/vagga/vagga-install.sh | sh')
 
 
 @task
@@ -44,7 +25,40 @@ def build_container(name):
         run('git clone {} .'.format(REPO))
         run('vagga _build {}'.format(name))
         rootfs = run('readlink -f .vagga/{}'.format(name))
+        run('cp -R * .vagga/{}/work/'.format(name))
         run('tar zcf {}.tar.gz -C {} ./'.format(name, rootfs))
+
+
+@task
+def deploy_last_container(name):
+    path = get_build_path(name)
+    version = get_container_version(name) + 1
+    target_path = '{}/{}/{}'.format(BASE_TARGET_PATH, name, version)
+    current_path = '{}/{}/{}'.format(BASE_TARGET_PATH, name, 'current')
+
+    if exists(target_path):
+        run('rm -rf {}'.format(target_path))
+    run('mkdir -p {}'.format(target_path))
+
+    run('tar xf {}/{}.tar.gz -C {}'.format(path, name, target_path))
+    run('rm -f {}'.format(current_path))
+    run('ln -sf {} {}'.format(target_path, current_path))
+
+    set_container_version(name, version)
+
+
+@task
+def deploy_container(name):
+    build_container(name)
+    deploy_last_container(name)
+
+
+@task
+def deploy():
+    deploy_container('postgres')
+    deploy_container('events_service')
+    run('systemctl restart systemd-nspawn@postgres.service; sleep 3')
+    run('systemctl restart systemd-nspawn@events_service.service')
 
 
 def get_build_path(name):
@@ -69,28 +83,19 @@ def get_version_file(name):
     return version_file
 
 
-@task
-def deploy_last_container(name):
-    path = get_build_path(name)
-    version = get_container_version(name) + 1
-    target_path = '{}/{}/{}'.format(BASE_TARGET_PATH, name, version)
-    current_path = '{}/{}/{}'.format(BASE_TARGET_PATH, name, 'current')
-
-    if exists(target_path):
-        run('rm -rf {}'.format(target_path))
-    run('mkdir -p {}'.format(target_path))
-
-    run('tar xf {}/{}.tar.gz -C {}'.format(path, name, target_path))
-    run('ln -sf {} {}'.format(target_path, current_path))
-
-    set_container_version(name, version)
+def require_packages(*names):
+    for name in names:
+        with settings(warn_only=True):
+            if run('dpkg -s {}'.format(name)).return_code != 0:
+                prepare_packages()
+                run('apt-get install -y {}'.format(name))
 
 
-@task
-def deploy_container(name):
-    build_container(name)
-    deploy_last_container(name)
-
+def require_vagga():
+    with settings(warn_only=True):
+        if run('which vagga').return_code != 0:
+            require_packages('curl')
+            run('curl http://files.zerogw.com/vagga/vagga-install.sh | sh')
 
 # systemd-nspawn -D /opt/postgres/current/ --setenv=PGDATA=/data --bind=/opt/postgres/data:/data chown postgres:postgres -R /data
 # systemd-nspawn -D /opt/postgres/current/ --setenv=PGDATA=/data --bind=/opt/postgres/data:/data su postgres -c '/usr/lib/postgresql/9.3/bin/pg_ctl initdb'
@@ -101,3 +106,16 @@ def deploy_container(name):
 # > su postgres -c '/usr/lib/postgresql/9.3/bin/postgres -h 127.0.0.1 -p 5433 -k /tmp -F' &
 # > su postgres -c "/usr/lib/postgresql/9.3/bin/psql -h 127.0.0.1 -p 5433 -c \"CREATE USER user WITH PASSWORD 'pwd';\""
 # > su postgres -c '/usr/lib/postgresql/9.3/bin/createdb -h 127.0.0.1 -p 5433 db -O user'
+
+# Adding machine to systemd boot
+# ln -s /opt/events_service/current /var/lib/machines/events_service
+# systemctl enable systemd-nspawn@events_service.service
+# systemctl edit --full systemd-nspawn@events_service.service
+#   put proper command like
+#     ExecStart=/usr/bin/systemd-nspawn --register=no --machine=%I --setenv=PGDATA=/data --bind=/opt/postgres/data:/data su postgres -c '/usr/lib/postgresql/9.3/bin/postgres -h 127.0.0.1 -p 5433-k /tmp -F'
+#   or
+#     systemd-nspawn -D /opt/events_service/current/ --register=no --setenv=DATABASE_URL=postgres://user:password@127.0.0.1:5433/db bash -c 'cd /work; alembic upgrade head'
+
+# alembic
+# ExecStartPre=/usr/bin/systemd-nspawn --register=no --machine=%I --setenv=DATABASE_URL=postgres://user:password@127.0.0.1:5433/db bash -c 'cd /work; alembic upgrade head'
+# /usr/bin/systemd-nspawn --register=no --machine=%I --register=no --setenv=DATABASE_URL=postgres://user:password@127.0.0.1:5433/db bash -c 'cd /work; python3 manage.py run_server'
