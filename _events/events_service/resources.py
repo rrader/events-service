30e7ca818138b15d63891722e1fc885ab3ea851b
Roman Rader <roman.rader@gmail.com>
2015-10-25 23:37:44 +0200

    Query API

--- _events/events_service/resources.py
+++ _events/events_service/resources.py
@@ -1,6 +1,9 @@
+import ast
 import asyncio
 import json
+import operator
 import uuid
+from aiohttp.web_exceptions import HTTPBadRequest
 from events_service.permissions import KeyProvided
 from rest_utils.resource import ModelResource, ModelBaseResource, CreateModelMixin
 from events_service.models import Event, EventsProvider
@@ -15,14 +18,53 @@ class EventValidator(ModelValidator):
         pass
 
 
+def parse_query(model, query):
+    operators = {
+        ast.And: operator.and_,
+        ast.Or: operator.or_,
+        ast.Eq: operator.eq,
+        ast.Gt: operator.gt,
+        ast.GtE: operator.ge,
+        ast.Lt: operator.lt,
+        ast.LtE: operator.le,
+    }
+
+    def _eval(node):
+        if isinstance(node, ast.Num):
+            return node.n
+        elif isinstance(node, ast.Str):
+            return node.s
+        elif isinstance(node, ast.Name):
+            return model.__mapper__.columns[node.id]
+        elif isinstance(node, ast.Compare):
+            if len(node.comparators) != 1 and len(node.ops) != 1:
+                raise TypeError(node)
+            return operators[type(node.ops[0])](_eval(node.left),
+                                                _eval(node.comparators[0]))
+        elif isinstance(node, ast.BoolOp):
+            return operators[type(node.op)](_eval(node.values[0]),
+                                            _eval(node.values[1]))
+        raise TypeError(node)
+    return _eval(ast.parse(query, mode='eval').body)
+
+
 class EventResource(ModelResource):
     model = Event
     permissions = [KeyProvided()]
     validator = EventValidator()
 
     def base_query(self, request):
-        return super().base_query(request).\
+        base_query = super().base_query(request).\
             where(Event.provider == request.events_provider.id)
+
+        query = request.GET.get('query')
+        if query:
+            try:
+                filter = parse_query(Event, query)
+            except (TypeError, SyntaxError) as e:
+                raise HTTPBadRequest(text=str(e))
+            base_query = base_query.where(filter)
+        return base_query
 
     def get_path(self):
         return r'/events'
